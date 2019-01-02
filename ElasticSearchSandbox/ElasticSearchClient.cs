edbb818760a83dad47534ebf22d83b571d49d466
Jay <jvanord@indasysllc.com>
2019-01-02 11:11:59 -0500

    initial commit

---
+++ ElasticSearchSandbox/ElasticSearchClient.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ElasticSearchSandbox
+{
+	public class ElasticSearchClient
+	{
+		private static readonly HttpClient _httpClient = new HttpClient();
+		private readonly Uri _queryUri;
+		private readonly string _queryBody;
+		public ElasticSearchClient(string queryUri, string queryBody = null)
+		{
+			_queryUri = new Uri(queryUri);
+			_queryBody = queryBody;
+		}
+		public async void Ping()
+		{
+			var request = (HttpWebRequest)HttpWebRequest.Create(_queryUri);
+			request.Timeout = 3000;
+			request.Method = "HEAD";
+			using (var response = await request.GetResponseAsync())
+			{
+				/* No Exception = Success */
+			}
+		}
+		public async Task<ElasticSearchResponse> Search() => await Search(false);
+		public async Task<ElasticSearchResponse> SearchWithScroll() => await Search(true);
+		private async Task<ElasticSearchResponse> Search(bool scroll)
+		{
+			var request = new HttpRequestMessage
+			{
+				RequestUri = _queryUri,
+				Method = HttpMethod.Get
+			};
+			if (!string.IsNullOrWhiteSpace(_queryBody))
+			{
+				request.Content = new StringContent(_queryBody);
+				request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			}
+			var result = await _httpClient.SendAsync(request).ConfigureAwait(false);
+			result.EnsureSuccessStatusCode();
+			var responseBody = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+			return ElasticSearchResponse.Parse(responseBody);
+		}
+	}
+	public class ElasticSearchResponse
+	{
+		private ElasticSearchResponse() { }
+
+		[JsonProperty("_scroll_id")]
+		public string ScrollId { get; private set; }
+
+		[JsonProperty("timed_out")]
+		public bool TimedOut { get; set; }
+
+		[JsonProperty("hits")]
+		public ElasticSearchHitsResult Hits { get; private set; }
+
+		public static ElasticSearchResponse Parse(string jsonResponseBody)
+		{
+			if (string.IsNullOrWhiteSpace(jsonResponseBody)) 
+				throw new ElasticSearchClientException("Cannot Evaluate Empty Response");
+			return JsonConvert.DeserializeObject<ElasticSearchResponse>(jsonResponseBody);
+		}
+	}
+
+	public class ElasticSearchHitsResult
+	{
+		[JsonProperty("total")]
+		public int Total { get; set; }
+
+		[JsonProperty("hits")]
+		public List<ElasticSearchHit> Hits { get; set; }
+	}
+
+	public class ElasticSearchHit
+	{
+		[JsonProperty("_id")]
+		public string Id { get; set; }
+
+		[JsonProperty("_index")]
+		public string Index { get; set; }
+
+		[JsonProperty("_type")]
+		public string Type { get; set; }
+
+		[JsonProperty("_score")]
+		public int Score { get; set; }
+
+		[JsonProperty("_source")]
+		public string RawSource { get; set; }
+
+		public T ParseSource<T>()
+		{
+			if (string.IsNullOrWhiteSpace(RawSource)) return default(T);
+			try
+			{
+				return JsonConvert.DeserializeObject<T>(RawSource);
+			}
+			catch
+			{
+				return default(T);
+			}
+		}
+	}
+
+	public class ElasticSearchClientException : Exception
+	{
+		public ElasticSearchClientException(string message) : base(message) { }
+	}
+}
---
+++ ElasticSearchSandbox/Program.cs
@@ -0,0 +1,168 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticSearchSandbox
+{
+	class Program
+	{
+		static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+		static async Task MainAsync(string[] args)
+		{
+			try
+			{
+				var esClient = new ElasticLowLevelClient(ElasticSearchConnectionSettings.LocalSimple.GetConfiguration());
+				var nestClient = new ElasticClient(ElasticSearchConnectionSettings.LocalSimple.GetNestConfiguration());
+
+				var mlesClient = new ElasticSearchClient("http://localhost:9200/companydatabase/_search");
+
+				return;
+
+				var esTest = await esClient.PingAsync<StringResponse>();
+				var nestTest = await nestClient.PingAsync();
+
+				var indices = await GetIndices(nestClient);
+				ShowResults(indices);
+
+				var repos = await GetRepositories(nestClient);
+				ShowResults(repos);
+
+				var master = await GetMaster(nestClient);
+				ShowResults(master);
+
+				var aliases = await GetAliases(nestClient);
+				ShowResults(aliases);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("FATAL ERROR: " + ex.Message);
+			}
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
+
+		static async Task<IEnumerable<CatIndicesRecord>> GetIndices(ElasticClient client)
+		{
+			var result = await client.CatIndicesAsync();
+			if (!result.IsValid)
+				throw new Exception("Result Invalid: " + result.OriginalException.Message);
+			return result.Records;
+		}
+		private static void ShowResults(IEnumerable<CatIndicesRecord> indices)
+		{
+			Console.WriteLine($"{indices.Count()} Indices Found");
+			foreach (var cat in indices)
+			{
+				Console.WriteLine($"{cat.Index} ({cat.DocsCount})");
+			}
+		}
+
+		static async Task<IEnumerable<CatRepositoriesRecord>> GetRepositories(ElasticClient client)
+		{
+			var result = await client.CatRepositoriesAsync();
+			if (!result.IsValid)
+				throw new Exception("Result Invalid: " + result.OriginalException.Message);
+			return result.Records;
+		}
+		private static void ShowResults(IEnumerable<CatRepositoriesRecord> repos)
+		{
+			Console.WriteLine($"{repos.Count()} Repositories Found");
+			foreach (var cat in repos)
+			{
+				Console.WriteLine($"{cat.Id} ({cat.Type})");
+			}
+		}
+
+		static async Task<IEnumerable<CatMasterRecord>> GetMaster(ElasticClient client)
+		{
+			var result = await client.CatMasterAsync();
+			if (!result.IsValid)
+				throw new Exception("Result Invalid: " + result.OriginalException.Message);
+			return result.Records;
+		}
+		private static void ShowResults(IEnumerable<CatMasterRecord> master)
+		{
+			Console.WriteLine($"{master.Count()} Master Fields Found");
+			foreach (var cat in master)
+			{
+				Console.WriteLine($"{cat.Node} ({cat.Id})");
+			}
+		}
+
+		static async Task<IEnumerable<CatAliasesRecord>> GetAliases(ElasticClient client)
+		{
+			var result = await client.CatAliasesAsync();
+			if (!result.IsValid)
+				throw new Exception("Result Invalid: " + result.OriginalException.Message);
+			return result.Records;
+		}
+		private static void ShowResults(IEnumerable<CatAliasesRecord> aliases)
+		{
+			Console.WriteLine($"{aliases.Count()} Aliases Found");
+			foreach (var alias in aliases)
+			{
+				Console.WriteLine($"{alias.Alias} ({alias.Index} | ({alias.SearchRouting})");
+			}
+		}
+
+	}
+	public class ElasticSearchConnectionSettings
+	{
+		public ElasticSearchConnectionSettings(string connectionUri)
+		{
+			ConnectionUris.Add(connectionUri);
+		}
+		public static ElasticSearchConnectionSettings LocalSimple => new ElasticSearchConnectionSettings("http://localhost:9200");
+
+		public List<string> ConnectionUris { get; set; } = new List<string>();
+		public bool PooledConnections => ConnectionUris.Count > 1;
+
+		public IConnectionConfigurationValues GetConfiguration()
+		{
+			if (ConnectionUris == null || ConnectionUris.Count < 1)
+			{
+				throw new Exception("At least one valid connection URI is required.");
+			}
+			if (PooledConnections)
+			{
+				return new ConnectionConfiguration(new SniffingConnectionPool(ConnectionUris.Select(u => new Uri(u))));
+			}
+			else
+			{
+				return new ConnectionConfiguration(new Uri(ConnectionUris.First()));
+			}
+		}
+
+		internal Nest.ConnectionSettings GetNestConfiguration()
+		{
+			if (ConnectionUris == null || ConnectionUris.Count < 1)
+			{
+				throw new Exception("At least one valid connection URI is required.");
+			}
+			if (PooledConnections)
+			{
+				return new Nest.ConnectionSettings(new SniffingConnectionPool(ConnectionUris.Select(u => new Uri(u))));
+			}
+			else
+			{
+				return new Nest.ConnectionSettings(new Uri(ConnectionUris.First()));
+			}
+		}
+	}
+	public class PersonPoco
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Designation { get; set; }
+		public decimal? Salary { get; set; }
+		public DateTime? LastDateOfJoiningName { get; set; }
+		public string Address { get; set; }
+		public string Gender { get; set; }
+		public int? Age { get; set; }
+		public string MaritalStatus { get; set; }
+		public string Interests { get; set; }
+	}
+}
